0533dff973b7c9eee0eab47d1ee66e116e783159
Kemal Kutay Furtun <kfurtun@gmail.com>
2022-01-17 16:11:28 -0800

    fix bundle output

--- packages/server/webpack.config.js
+++ packages/server/webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
   target: "node",
   entry: "./src/app.js",
   output: {
-    filename: "main.js",
+    filename: "index.js",
     path: path.resolve(__dirname, "build"),
   },
 };
